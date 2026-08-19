6c54ea30891949331eff2d4c0003ef1b7939510e
RomanL1 <40143010+RomanL1@users.noreply.github.com>
2026-08-19 22:23:53 +0200

    chore: remove public deployment visibility, improve vercel deployment

--- api/index.js
+++ api/index.js
@@ -1,2 +1,8 @@
-export default import('../dist/personal-blog-resume/server/server.mjs')
-  .then(module => module.app());
+let appPromise;
+
+module.exports = async function handler(req, res) {
+  appPromise ??= import('../dist/personal-blog-resume/server/server.mjs')
+    .then(({ app }) => app());
+
+  return (await appPromise)(req, res);
+};
